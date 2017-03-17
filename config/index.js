f588126790ee88f6e27ab5c73fba3a512ce69773
neusc <neusc829@163.com>
2017-03-17 18:31:56 +0800

    Initial commit

---
+++ config/index.js
@@ -0,0 +1,51 @@
+// see http://vuejs-templates.github.io/webpack for documentation.
+var path = require('path')
+var glob = require('glob')
+
+// var getDist = function () {
+//   var files = glob.sync('./src/js/*/index.js');
+//   var newDist = [];
+//
+//   files.forEach(function(f){
+//     var name = /js\/(.*?)\/index\.js/.exec(f)[1];//得到js下的目录名,.*?表示懒惰模式，匹配最短
+//     newDist.push(name);
+//   });
+//   console.log(newDist);
+//   return newDist;
+// };
+
+module.exports = {
+  build: {
+    env: require('./prod.env'),
+    index: path.resolve(__dirname, '../dist/index.html'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    // Gzip off by default as many popular static hosts such as
+    // Surge or Netlify already gzip all static assets for you.
+    // Before setting to `true`, make sure to:
+    // npm install --save-dev compression-webpack-plugin
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    // Run the build command with an extra argument to
+    // View the bundle analyzer report after build finishes:
+    // `npm run build --report`
+    // Set to `true` or `false` to always turn it on or off
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
+  dev: {
+    env: require('./dev.env'),
+    port: 8080,
+    autoOpenBrowser: true,
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    // CSS Sourcemaps off by default because relative paths are "buggy"
+    // with this option, according to the CSS-Loader README
+    // (https://github.com/webpack/css-loader#sourcemaps)
+    // In our experience, they generally work as expected,
+    // just be aware of this issue when enabling this option.
+    cssSourceMap: false
+  }
+}
---
+++ src/js/sell/components/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="sell">
+    <img src="../../../image/sell/logo-sell.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
+
+<style>
+  #sell {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
+

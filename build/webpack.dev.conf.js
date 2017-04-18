cf399894bf8ed957462e48c2b5361d66f520ef54
neusc <neusc829@163.com>
2017-04-18 17:15:48 +0800

    HtmlWebpackPlugin插件对于多入口html文件注入更改为智能添加模式

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -2,9 +2,27 @@ var utils = require('./utils')
 var webpack = require('webpack')
 var config = require('../config')
 var merge = require('webpack-merge')
+var glob = require('glob')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+
+/**
+ * 动态获取所有项目目录名称
+ */
+
+var getDir = function () {
+  var files = glob.sync('./src/js/*/index.js');
+  var directories = [];
+
+  files.forEach(function(f){
+    var name = /js\/(.*?)\/index\.js/.exec(f)[1];//得到demo这样的项目目录名,.*?表示懒惰模式，匹配最短
+    directories.push(name);
+  });
+  console.log(directories);
+  return directories;
+};
+
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -24,8 +42,9 @@ module.exports = merge(baseWebpackConfig, {
     // https://github.com/glenjamin/webpack-hot-middleware#installation--usage
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
+    
     // https://github.com/ampedandwired/html-webpack-plugin
-
+    /*
     new HtmlWebpackPlugin({
       filename: '../dist/static/view/demo/index.html',
       template: 'src/js/demo/index.html',
@@ -38,6 +57,19 @@ module.exports = merge(baseWebpackConfig, {
       inject: true,
       chunks:['sell/index','vendor','manifest'],
     }),
+    */
     new FriendlyErrorsPlugin()
   ]
-})
+});
+
+/*处理html*/
+for(var dir of getDir()){
+  var conf = {
+    filename: '../dist/static/view/' + dir +'/index.html',
+    template: 'src/js/' + dir + '/index.html',
+    inject: true,
+    chunks: [dir + '/index', 'vendor', 'manifest'],
+  };
+
+  module.exports.plugins.push(new HtmlWebpackPlugin(conf));
+}
